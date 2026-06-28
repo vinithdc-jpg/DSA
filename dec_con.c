2cc0d8a655d2cffedb3f7e7780f21e3c6b5d545b
vinith <vinithdc@gmail.com>
2026-06-28 21:03:59 +0530

    Add decimal to binary conversion program

---
+++ dec_con.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    int num, binary[32], i = 0;
+
+    printf("Enter number: ");
+    scanf("%d",&num);
+
+    while(num>0){
+        binary[i]=num%2;
+        num/=2;
+        i++;
+    }
+
+    printf("Binary = ");
+
+    for(int j=i-1;j>=0;j--)
+        printf("%d",binary[j]);
+
+    return 0;
+}
