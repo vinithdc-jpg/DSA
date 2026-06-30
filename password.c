33d0e0b832dad676f5c5f8b2632dbed6248b66bd
vinith <vinithdc@gmail.com>
2026-06-30 21:45:09 +0530

    password

---
+++ password.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+    char chars[] =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789"
+        "@#$%&*!?";
+
+    int length;
+
+    printf("Enter password length: ");
+    scanf("%d", &length);
+
+    srand(time(NULL));
+
+    printf("\nGenerated Password: ");
+
+    for (int i = 0; i < length; i++) {
+
+        int index = rand() % (sizeof(chars) - 1);
+
+        printf("%c", chars[index]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
