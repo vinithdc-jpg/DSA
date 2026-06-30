03cddb1b9efde4010422ca925ca82f3eb68535c2
vinith <vinithdc@gmail.com>
2026-06-30 21:47:59 +0530

    Encryption

---
+++ encription.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char text[100];
+    int shift;
+
+    printf("Enter Text: ");
+    fgets(text, sizeof(text), stdin);
+
+    printf("Enter Shift Value: ");
+    scanf("%d", &shift);
+
+    // Encryption
+    for(int i = 0; text[i] != '\0'; i++) {
+
+        char ch = text[i];
+
+        if(ch >= 'A' && ch <= 'Z') {
+
+            text[i] = ((ch - 'A' + shift) % 26) + 'A';
+
+        }
+
+        else if(ch >= 'a' && ch <= 'z') {
+
+            text[i] = ((ch - 'a' + shift) % 26) + 'a';
+
+        }
+    }
+
+    printf("\nEncrypted Text: %s", text);
+
+    // Decryption
+    for(int i = 0; text[i] != '\0'; i++) {
+
+        char ch = text[i];
+
+        if(ch >= 'A' && ch <= 'Z') {
+
+            text[i] = ((ch - 'A' - shift + 26) % 26) + 'A';
+
+        }
+
+        else if(ch >= 'a' && ch <= 'z') {
+
+            text[i] = ((ch - 'a' - shift + 26) % 26) + 'a';
+
+        }
+    }
+
+    printf("\nDecrypted Text: %s", text);
+
+    return 0;
+}
