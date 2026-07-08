f495d0009a7e671c2acbafd5e8ada0f2b8767e19
vinith <vinithdc@gmail.com>
2026-07-08 20:35:52 +0530

    Convert Lowercase to Uppercase

---
+++ lower_upper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] -= 32;
+    }
+
+    printf("Uppercase: %s", str);
+
+    return 0;
+}
