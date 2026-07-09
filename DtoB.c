7e349fb97a619397f6896b5d050fc8bba0bdd0c3
vinith <vinithdc@gmail.com>
2026-07-09 21:36:19 +0530

    SM

--- DtoB.c
+++ DtoB.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int n, stack[32], top = -1;
 
     printf("Enter Number: ");
     scanf("%d", &n);
 
-    while (n > 0) {
+    while (n > 0)
+    {
         stack[++top] = n % 2;
         n /= 2;
     }
