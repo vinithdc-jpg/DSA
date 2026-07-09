f2af6132574a5338aeeddd2d40bd47f24874a53b
vinith <vinithdc@gmail.com>
2026-07-09 21:34:32 +0530

    Decimal to Binary Using Stack

---
+++ DtoB.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, stack[32], top = -1;
+
+    printf("Enter Number: ");
+    scanf("%d", &n);
+
+    while (n > 0) {
+        stack[++top] = n % 2;
+        n /= 2;
+    }
+
+    printf("Binary: ");
+    while (top != -1)
+        printf("%d", stack[top--]);
+
+    return 0;
+}
