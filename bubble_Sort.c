dfd5d612aebb2794af4a18862ecfd9c6bf57443b
vinith <vinithdc@gmail.com>
2026-07-19 19:24:45 +0530

    Changes 1

--- bubble_Sort.c
+++ bubble_Sort.c
@@ -1,3 +1,4 @@
+//Bubble sort
 #include <stdio.h>
 
 int main() {
@@ -7,6 +8,7 @@ int main() {
     for(int i = 0; i < n - 1; i++) {
         for(int j = 0; j < n - i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
+                //Swap the element
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
